685193f70c4d4d27a67062e3b994f32ef4127585
ryhaldenys <denysryhal24@gmail.com>
2023-02-09 14:12:03 +0200

    add back-end directory

---
+++ back-end/poems-back-end/src/main/java/ua/poems_club/PoemsBackEndApplication.java
@@ -0,0 +1,13 @@
+package ua.poems_club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoemsBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoemsBackEndApplication.class, args);
+    }
+
+}
---
+++ back-end/poems-back-end/src/test/java/ua/poems_club/PoemsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.poems_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoemsBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
